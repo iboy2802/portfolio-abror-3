73ce98f34a324c8e549e345d43ef64768a14c4db
iboy2802 <abrormhd6821@gmail.com>
2025-06-23 16:42:29 +0700

    Update sparkle.js

--- sparkle.js
+++ sparkle.js
@@ -1,8 +1,13 @@
+const colors = ['#6A0DAD', '#191970', '#8B0000', '#2a2f36', '#00FF9F'];
+
 document.addEventListener('mousemove', e => {
-  const s = document.createElement('div');
-  s.className = 'sparkle';
-  s.style.left = `${e.pageX}px`;
-  s.style.top = `${e.pageY}px`;
-  document.body.appendChild(s);
-  setTimeout(() => s.remove(), 600);
+  const sparkle = document.createElement('div');
+  sparkle.classList.add('sparkle');
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  sparkle.style.background = color;
+  sparkle.style.boxShadow = `0 0 10px ${color}, 0 0 20px ${color}`;
+  sparkle.style.left = `${e.pageX}px`;
+  sparkle.style.top = `${e.pageY}px`;
+  document.body.appendChild(sparkle);
+  setTimeout(() => sparkle.remove(), 600);
 });
