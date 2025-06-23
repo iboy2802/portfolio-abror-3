0c45f7d0d7bcba56578360e849fc02601ca6c391
iboy2802 <abrormhd6821@gmail.com>
2025-06-23 16:23:12 +0700

    Create  sparkle.js

---
+++ assets/js/sparkle.js
@@ -0,0 +1,9 @@
+// sparkle.js
+document.addEventListener('mousemove', function(e) {
+  const sparkle = document.createElement('div');
+  sparkle.classList.add('sparkle');
+  sparkle.style.left = `${e.pageX}px`;
+  sparkle.style.top = `${e.pageY}px`;
+  document.body.appendChild(sparkle);
+  setTimeout(() => sparkle.remove(), 600);
+});
